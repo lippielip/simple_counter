b82a9acefd6d285fc357cbfeaea2cb5b31747f61
lippielip <lippiebraun@gmail.com>
2020-11-02 22:25:21 +0100

    fix CSS bugs on mobile

--- src/components/render/stats.js
+++ src/components/render/stats.js
@@ -4,14 +4,7 @@ export default class StatsPage extends React.Component {
 	render () {
 		return (
 			<div id="StatsPage" className="statPage">
-				<canvas id="clicksPerDay" width="400" height="400" />
-				<canvas id="clicksPerMonth" width="400" height="400" />
 				<h1>*Statistics Page WIP*</h1>
-				<canvas id="totalClicks" width="400" height="400" />
-				<canvas id="osType" width="400" height="400" />
-				<canvas id="deviceType" width="400" height="400" />
-				<canvas id="location" width="400" height="400" />
-				<canvas id="language" width="400" height="400" />
 			</div>
 		);
 	}
